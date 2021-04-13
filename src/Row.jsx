d06f3752cc09b2dc637b4a87c48fc3a75c816507
kgaurav123 <kumar123gaurav.kg@gmail.com>
2021-04-13 23:25:23 +0530

    home page completed

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "./axios"
 import "./row.css"
-function Row({ title }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
+    const [movie, setMovie] = useState([]);
+    const base_url = "https://image.tmdb.org/t/p/original/";
+    useEffect(() => {
+        async function fetchdata() {
+            const request = await axios.get(fetchUrl);
+            setMovie(request.data.results);
+            return request;
+        }
+        fetchdata();
+    }, [fetchUrl]);
+    console.log(movie)
     return (
         <div className="row">
             <h2>{title}</h2>
+            <div className="row_posters">
+                {movie.map(movie => (
+                    <img className={`row_poster ${isLargeRow && "row_posterLarge"}`} key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+
+                ))}
+            </div>
         </div>
     )
 }
